e6da960e8c38ade6e729e6e87be58af5e8652aca
Rodrigo Magalhães <magalhaesrda@gmail.com>
2020-07-01 00:46:40 -0300

    Iniciando o exercício número 2

---
+++ ex02/ex02.js
@@ -0,0 +1,18 @@
+
+
+function contador(){
+    var msg = document.getElementById('msg')
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var mao = "👉"
+    var mulher = "🙅"
+    var lista =[]
+    for(i = inicio; i <= fim; i = i + passo){
+       
+        
+    }
+
+
+}
+
